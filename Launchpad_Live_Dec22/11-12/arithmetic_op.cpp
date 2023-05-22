b2e1ead49afe74c01570b2038cd791f34f0e9116
user992199 <109051881+user992199@users.noreply.github.com>
2023-05-22 15:27:34 +0530

    Delete Launchpad_Live_Dec22 directory

--- Launchpad_Live_Dec22/11-12/arithmetic_op.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	int i = 100+7/100*8263-98%23+76;
-	cout<<i<<endl;
-	
-	cout<<(100+7)/3<<endl;
-	return 0;
-}
--- Launchpad_Live_Dec22/11-12/data_mod.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-	long double i;
-	cout<<sizeof(i)<<endl;
-	return 0;
-}
